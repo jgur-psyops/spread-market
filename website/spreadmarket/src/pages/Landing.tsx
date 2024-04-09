1ff7fcbe6a01fd6f01e4f2ca471a8a1713619807
DocBadTouch <36558871+DocBadTouch@users.noreply.github.com>
2024-04-08 22:09:07 -0400

    Update landing page background and images

--- website/spreadmarket/src/pages/Landing.tsx
+++ website/spreadmarket/src/pages/Landing.tsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
 
+const landingBg =
+  "linear-gradient(to bottom right, rgba(199, 42, 183, 0.20) 0%, rgba(42, 171, 180, 0.20) 20%, rgba(237, 214, 5, 0.20) 50%) bottom right / 50% 50% no-repeat, linear-gradient(to bottom left, rgba(199, 42, 183, 0.20) 0%, rgba(42, 171, 180, 0.20) 20%, rgba(237, 214, 5, 0.20) 50%) bottom left / 50% 50% no-repeat, linear-gradient(to top left, rgba(199, 42, 183, 0.20) 0%, rgba(42, 171, 180, 0.20) 20%, rgba(237, 214, 5, 0.20) 50%) top left / 50% 50% no-repeat, linear-gradient(to top right, rgba(199, 42, 183, 0.20) 0%, rgba(42, 171, 180, 0.20) 20%, rgba(237, 214, 5, 0.20) 50%) top right / 50% 50% no-repeat, radial-gradient(72.74% 40.15% at 85.56% 81.35%, rgba(24, 20, 188, 0.20) 0%, rgba(251, 40, 255, 0.20) 52.5%, rgba(170, 16, 16, 0.20) 100%), radial-gradient(140.63% 111.8% at 0% 0%, #8432C5 0%, rgba(16, 177, 148, 0.96) 41.5%, #020068 100%)";
 export const Landing = () => {
   return (
     <div
-      className="w-full h-full flex flex-col items-center  bg bg-cover bg-center bg-no-repeat bg-fixed"
-      style={{ backgroundImage: "url(/assets/LandingBg.png)" }}
+      className="w-full h-full flex flex-col items-center  bg bg-cover bg-center bg-no-repeat bg-fixed min-h-[100vh]"
+      style={{
+        //backgroundImage: "url(/assets/LandingBg.png)",
+        background: landingBg,
+      }}
     >
-      <div className="w-full h-full flex flex-col items-center justify-center max-w-[1200px]">
+      <div className="w-full h-full flex flex-col items-center justify-center max-w-[1200px] mb-50px">
         <Hero />
         <HowItWorks />
         <ConnectWithUs />
@@ -70,7 +75,7 @@ const subTitleStyle = {
 
 export const Hero = () => {
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center mt-[145px] mb-[20px]">
+    <div className="w-full h-full flex flex-col items-center justify-center mt-[125px] mb-[20px]">
       <div className="w-full flex items-center justify-around ">
         <div className="flex-col justify-center items-center gap-10 inline-flex py-10">
           <div style={titleStyle}>sBREAD FINANCE</div>
@@ -108,7 +113,11 @@ export const Hero = () => {
             </div>
           </div>
         </div>
-        <img className="smiley-toast-animation" src="/assets/toasty.svg" alt="Smiley Toast" />
+        <img
+          className="smiley-toast-animation"
+          src="/assets/toasty2.svg"
+          alt="Smiley Toast"
+        />
       </div>
     </div>
   );
@@ -116,8 +125,8 @@ export const Hero = () => {
 
 export const HowItWorks = () => {
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center">
-      <div className="h-28 flex-col justify-start items-start gap-2.5 inline-flex">
+    <div className="w-full h-full flex flex-col items-center justify-center gap-10">
+      <div className=" flex-col justify-start items-start gap-2.5 inline-flex">
         <div className="whitespace-nowrap text-center text-white text-opacity-97 text-6xl font-bold font-['Poppins'] leading-10">
           How does it work?
         </div>
@@ -126,11 +135,13 @@ export const HowItWorks = () => {
         </div>
       </div>
       <hr className="hr-line-break"></hr>
-      <div className="w-full h-80 justify-around items-center inline-flex">
+      <div className="w-full justify-around items-center inline-flex">
         <HowItWorksCard
           title="Fly like a metal bird"
           textBody="Your assets fly like an Eagle (well, like a Condor anyways)."
           imageFileName="iron-condor.svg"
+          link1={{ href: "/vaultList", title: "Get Started" }}
+          link2={{ href: "/vaultList", title: "Learn More" }}
         />
         <HowItWorksCard
           title="Profit off volatility, don't cry about it."
@@ -141,18 +152,25 @@ export const HowItWorks = () => {
     </div>
   );
 };
-
+type LinkProps = {
+  href: string;
+  title: string;
+};
 export const HowItWorksCard = ({
   title,
   textBody,
   imageFileName,
+  link1,
+  link2,
 }: {
   title: string;
   textBody: string;
   imageFileName: string;
+  link1?: LinkProps;
+  link2?: LinkProps;
 }) => {
   return (
-    <div className="w-96 p-7 bg-gradient-to-b from-indigo-400 to-slate-900 rounded-2xl flex-col justify-start items-start gap-6 inline-flex">
+    <div className="w-[calc(35vmin)] p-7 bg-gradient-to-b from-indigo-400 to-slate-900 rounded-2xl flex-col justify-start items-start gap-6 inline-flex self-stretch">
       <div className="flex-col justify-start items-start gap-5 flex">
         <div className="text-center text-white text-base font-semibold font-['Poppins'] leading-tight">
           {title}
@@ -165,20 +183,28 @@ export const HowItWorksCard = ({
         </div>
       </div>
       <div className="justify-center items-center gap-5 inline-flex">
-        <div className="px-3 py-2.5 rounded-lg border border-slate-400 justify-center items-center gap-1 flex">
-          <div className="justify-start items-center gap-2.5 flex">
-            <div className="text-center text-slate-300 text-sm font-bold font-['Inter'] leading-none">
-              Read more
+        {link1 && (
+          <div className="px-3 py-2.5 rounded-lg border border-slate-400 justify-center items-center gap-1 flex">
+            <div className="justify-start items-center gap-2.5 flex">
+              <Link to={link1?.href} className="text-inherit">
+                <div className="text-center text-slate-300 text-sm font-bold font-['Inter'] leading-none">
+                  {link1?.title}
+                </div>
+              </Link>
             </div>
           </div>
-        </div>
-        <div className="px-3 py-2.5 bg-indigo-100 rounded-lg shadow border border-indigo-300 justify-center items-center gap-1 flex">
-          <div className="justify-start items-center gap-2.5 flex">
-            <div className="text-center text-slate-900 text-sm font-bold font-['Inter'] leading-none">
-              Stake now
+        )}
+        {link2 && (
+          <div className="px-3 py-2.5 bg-indigo-100 rounded-lg shadow border border-indigo-300 justify-center items-center gap-1 flex">
+            <div className="justify-start items-center gap-2.5 flex">
+              <Link to={link2.href}>
+                <div className="text-center text-slate-900 text-sm font-bold font-['Inter'] leading-none">
+                  {link2.title}
+                </div>
+              </Link>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
